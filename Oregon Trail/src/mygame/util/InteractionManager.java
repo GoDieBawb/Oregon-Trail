8ae99e022c7f7dd2470fde8c05ded8ee30675869
BigBawb <meancraft@hotmail.com>
2015-02-12 15:57:28 -0800

    Utility Work
    Added Input listening
    Added YamlManager
    Some work on the utility manager

---
+++ Oregon Trail/src/mygame/util/InteractionManager.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame.util;
+
+import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
+
+/**
+ *
+ * @author Bawb
+ */
+public class InteractionManager implements ActionListener {
+    
+    private InputManager inputManager;
+    private boolean      up = false, down = false, left = false, right = false, click = false;
+    
+    public InteractionManager(InputManager inputManager) {
+        this.inputManager = inputManager;
+        setUpKeys();
+    }
+    
+    private void setUpKeys() {
+        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Click", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
+        inputManager.addListener(this, "Up");
+        inputManager.addListener(this, "Down");
+        inputManager.addListener(this, "Left");
+        inputManager.addListener(this, "Right");
+        inputManager.addListener(this, "Click");
+    }
+    
+    public void onAction(String binding, boolean isPressed, float tpf) {
+        
+        if (binding.equals("Up")) {
+            up = isPressed;
+        }
+        
+        else if (binding.equals("Down")) {
+            down = isPressed;
+        }
+        
+        else if (binding.equals("Left")) {
+            left = isPressed;
+        }
+        
+        else if (binding .equals("Right")) {
+            right = isPressed;
+        }
+        
+        else if (binding.equals("Click")) {
+            click = isPressed;
+        }
+        
+    }
+    
+    public boolean getIsPressed(String triggerName) {
+        
+        if (triggerName.equals("Left"))
+        return left;
+        else if (triggerName.equals("Right"))
+        return right;
+        else if (triggerName.equals("Up"))
+        return up;
+        else if (triggerName.equals("Down"))
+        return down;
+        else if (triggerName.equals("Click"))
+        return click;    
+        else
+        return false;
+        
+    }
+    
+}
--- Oregon Trail/src/mygame/util/UtilityManager.java
+++ Oregon Trail/src/mygame/util/UtilityManager.java
@@ -4,10 +4,43 @@
  */
 package mygame.util;
 
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+
 /**
  *
  * @author Bawb
  */
-public class UtilityManager {
+public class UtilityManager extends AbstractAppState {
+    
+    private InteractionManager interactionManager;
+    private SimpleApplication  app;
+    private MaterialManager    matMan;
+
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        this.app = (SimpleApplication) app;
+        createInteractionManager();
+        createMaterialManager();
+    }
+
+    private void createMaterialManager() {
+       matMan = new MaterialManager(app.getAssetManager());
+    }
+    
+    public MaterialManager getMaterialManager() {
+        return matMan;
+    }
+    
+    private void createInteractionManager() {
+        interactionManager = new InteractionManager(app.getInputManager());
+    }
+    
+    public InteractionManager getInteractionManager() {
+        return interactionManager;
+    }
     
 }
