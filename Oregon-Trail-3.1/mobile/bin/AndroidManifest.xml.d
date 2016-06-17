b6af7369029f20148a79beeb43c017cff7dfa239
root <root@bob-desktop>
2016-06-17 09:14:07 -0700

    Upgrade to 3.1

--- Oregon-Trail-3.1/mobile/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/bob/Documents/Oregon-Trail-3.1/mobile/bin/AndroidManifest.xml : \
-/home/bob/Documents/Oregon-Trail-3.1/mobile/AndroidManifest.xml \
--- Oregon-Trail-3.1/mobile/gen/com/bigbawb/oregontrail/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.bigbawb.oregontrail;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = false;
-}
